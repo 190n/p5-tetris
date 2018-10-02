7071389ba426f468a5c7df9baf8d909a7e6776c0
190n <benjamin.j.grant@gmail.com>
2018-10-02 14:34:19 -0700

    Test by drawing every shape in every orientation

--- sketch.js
+++ sketch.js
@@ -6,7 +6,8 @@ const shapes = 'ijlostz'.split(''),
         o: '#00ffff',
         s: '#00ff00',
         t: '#800080',
-        z: '#ff0000'
+        z: '#ff0000',
+        '': '#ffffff'
     },
     scale = 16,
     size = 32,
@@ -15,11 +16,31 @@ const shapes = 'ijlostz'.split(''),
 function setup() {
     createCanvas(scale * size, scale * size);
     noStroke();
+
+    // create empty board
     for (let x = 0; x < size; x++) {
         board.push([]);
         for (let y = 0; y < size; y++) {
-            board[x].push(random(shapes));
+            board[x].push('');
         }
+    }
+
+    let x = 5, y = 5;
+    for (let s of shapes) {
+        for (let o in orientations[s]) {
+            placeShape(x, y, s, o);
+            x += 5;
+            if (x > 25) {
+                x = 5;
+                y += 5;
+            }
+        }
+    }
+}
+
+function placeShape(x, y, shape, o) {
+    for (let point of orientations[shape][o]) {
+        board[x + point[0]][y + point[1]] = shape;
     }
 }
 
